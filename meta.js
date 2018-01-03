4e59f4a3479e5f460317251b8c8d69a1194868df
joshua1988 <jangkeehyo@gmail.com>
2018-01-03 11:19:33 +0900

    Added the test code on the custom cli template

---
+++ meta.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name????"
+    },
+    "short_name": {
+      "type": "string",
+      "required": false,
+      "message": "Project short name: fewer than 12 characters to not be truncated on homescreens (default: same as name)"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A Vue.js project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    }
+  },
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+};
